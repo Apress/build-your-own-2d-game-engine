f528539a6c4c8c6eb6b36b0a36a4871e9fff7ddf
kelvinhsung <ksung@u.washington.edu>
2020-01-03 22:18:31 -0800

    Remove student projectts and redirect links

--- AdditionalMaterials/StudentProjects/2019.7.NUS/4.Group2/GameSrc/README.md
+++
@@ -1,2 +0,0 @@
-# ArrowOfLove
-# TheArrowOfLove-V3
--- AdditionalMaterials/StudentProjects/2019.7.NUS/4.Group2/GameSrc/src/MyGame/Objects/PlatformSet.js
+++
@@ -1,6 +0,0 @@
-//The set of all the platforms
-
-function PlatformSet() {
-    GameObjectSet.call(this);
-}
-gEngine.Core.inheritPrototype(PlatformSet,GameObjectSet);
